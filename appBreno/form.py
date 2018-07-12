07e92d1013938045aa819be2abe52f09ac7a14e0
Brunowf <brunoweiller@hotmail.com>
2018-07-12 16:08:18 -0300

    Alterando css do form

--- appBreno/form.py
+++ appBreno/form.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Tabela
 
 
@@ -6,3 +7,9 @@ class CadastroForm(ModelForm):
     class Meta:
         model = Tabela
         fields = ['nome', 'cpf', 'email', 'telefone']
+        widgets = {
+            'nome': forms.TextInput(attrs={'class':'w3-input w3-border w3-text-blue'}),
+            'cpf': forms.TextInput(attrs={'class': 'w3-input w3-border w3-text-blue'}),
+            'email': forms.TextInput(attrs={'class': 'w3-input w3-border w3-text-blue'}),
+            'telefone': forms.TextInput(attrs={'class': 'w3-input w3-border w3-text-blue'})
+        }
